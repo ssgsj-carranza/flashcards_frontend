e5db2e2d826d86e774d486e85eee9219ab4dc656
unknown <ssgsj.carranza@gmail.com>
2021-05-04 13:32:44 -0500

    changed up navbar a bit trying to play with something new

--- flashcards-frontend/src/components/TitleBar/TitleBar.js
+++ flashcards-frontend/src/components/TitleBar/TitleBar.js
@@ -1,15 +1,66 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+// import './TitleBar.css';
+import styled from 'styled-components';
+import {NavLink as Link} from 'react-router-dom';
+import {FaBars} from 'react-icons/fa';
 
-function TitleBar(props) {
+// function TitleBar(props) {
+//     return (
+//         <div className="row row-spacer">
+//             <div className="col-md-12" style={{padding:0}}>
+//                 <div className="titlebar-nav">
+//                     <h1>Not Quizlet</h1>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default TitleBar;
+
+const Navbar = () => {
     return (
-        <div className="row row-spacer">
-            <div className="col=md=12" style={{padding:0}}>
-                <div>
-                    <h1>Not Quizlet</h1>
-                </div>
-            </div>
-        </div>
+        <>
+        <Nav>
+            <NavLink to="/">
+                <img src="" alt="" />
+            </NavLink>
+            <Bars />
+            <NavMenu>
+                <NavLink to="/home" activeStyle>
+                    Home
+                </NavLink>
+            </NavMenu>
+        </Nav>
+        </>
     );
-}
+};
+export default Navbar
 
-export default TitleBar;
+export const Nav = styled.nav`
+    background: #000;
+    height: 80px;
+    display: flex;
+    justify-content: space-between;
+    padding: 0.5rem calc((100vw - 1000px) / 2);
+    z-index: 10;
+`
+
+export const NavLink = styled(Link)`
+    color: #fff;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+
+    &.active{
+       color: #15cdfc 
+    }
+`
+
+export const Bars = styled
+
+// export default Navbar
