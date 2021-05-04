78f8bea805c6e8df410287bc9c1eb8804b5ac65f
unknown <ssgsj.carranza@gmail.com>
2021-05-04 14:30:56 -0500

    created function to go to the next card in the stack

--- flashcards-frontend/src/components/TitleBar/TitleBar.js
+++ flashcards-frontend/src/components/TitleBar/TitleBar.js
@@ -1,66 +1,21 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-// import './TitleBar.css';
-import styled from 'styled-components';
-import {NavLink as Link} from 'react-router-dom';
-import {FaBars} from 'react-icons/fa';
+// import { NavLink } from 'react-router-dom';
+import './TitleBar.css';
+// import styled from 'styled-components';
+// import {NavLink as Link} from 'react-router-dom';
+// import {FaBars} from 'react-icons/fa';
 
-// function TitleBar(props) {
-//     return (
-//         <div className="row row-spacer">
-//             <div className="col-md-12" style={{padding:0}}>
-//                 <div className="titlebar-nav">
-//                     <h1>Not Quizlet</h1>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default TitleBar;
-
-const Navbar = () => {
+function TitleBar(props) {
     return (
-        <>
-        <Nav>
-            <NavLink to="/">
-                <img src="" alt="" />
-            </NavLink>
-            <Bars />
-            <NavMenu>
-                <NavLink to="/home" activeStyle>
-                    Home
-                </NavLink>
-            </NavMenu>
-        </Nav>
-        </>
+        <div className="row row-spacer">
+            <div className="col-md-12" style={{padding:0}}>
+                <div className="titlebar-nav">
+                    <h1>Not Quizlet</h1>
+                </div>
+            </div>
+        </div>
     );
-};
-export default Navbar
+}
 
-export const Nav = styled.nav`
-    background: #000;
-    height: 80px;
-    display: flex;
-    justify-content: space-between;
-    padding: 0.5rem calc((100vw - 1000px) / 2);
-    z-index: 10;
-`
+export default TitleBar;
 
-export const NavLink = styled(Link)`
-    color: #fff;
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    padding: 0 1rem;
-    height: 100%;
-    cursor: pointer;
-
-    &.active{
-       color: #15cdfc 
-    }
-`
-
-export const Bars = styled
-
-// export default Navbar
