6cbc3eeb495db1e0a761e1cb137d9204a16878dd
unknown <ssgsj.carranza@gmail.com>
2021-05-04 14:39:29 -0500

    created previous card function and buttons

--- flashcards-frontend/src/apps.js
+++ flashcards-frontend/src/apps.js
@@ -3,13 +3,47 @@ import TitleBar from './components/TitleBar/TitleBar'
 
 class App extends Component {
     state = {
-        flashcards: [],
-        collection: []
+            flashcards: [],
+            collection: [],
+            cardNumber: 0
+        };
+    
+    
+    goToNextCard(){
+        let tempCardNumber = this.state.cardNumber;
+        tempCardNumber ++;
+        if(tempCardNumber === this.flashcards.length){
+            tempCardNumber = 0;
+        }
+        this.setState({
+            cardNumber:tempCardNumber
+        });
+    }
+
+    goToPreviousCard(){
+        let tempCardNumber = this.state.cardNumber;
+        tempCardNumber --;
+        if(tempCardNumber < 0)
+            tempCardNumber = this.flashcards.length - 1;
+        this.setState({
+            cardNumber: tempCardNumber
+        });
     }
     render() {
         return (
             <div className="container-fluid">
                 <TitleBar />
+                <div className="row row-spacer">
+                    <div className="col-md-4">
+                        <button onClick={() => this.goToPreviousCard()}>Previous Card</button>
+                    </div>
+                    <div className="col-md-4">
+                        {/* display card */}
+                    </div>
+                    <div className="col-md-4">
+                        <button onClick={() => this.goToNextCard()}>Next Card</button>
+                    </div>
+                </div>
             </div>
         );
     }
