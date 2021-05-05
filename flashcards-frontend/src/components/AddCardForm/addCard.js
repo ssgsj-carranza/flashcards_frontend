20e2e7cd8418669ed83391d6e0e436bcc433ae77
unknown <ssgsj.carranza@gmail.com>
2021-05-05 14:51:24 -0500

    ran into issues with my previous and next buttons, researching more methods

--- flashcards-frontend/src/components/AddCardForm/addCard.js
+++ flashcards-frontend/src/components/AddCardForm/addCard.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Segment, Button, Form} from 'semantic-ui-react';
+import {Segment, Button, Form, FormInput} from 'semantic-ui-react';
+import styled from 'styled-components';
 
 class AddCard extends Component {
     constructor(props) {
@@ -19,5 +20,65 @@ class AddCard extends Component {
     }
     onSubmit(event){
         event.preventDefault();
+        const card = {
+            front: this.state.front,
+            back: this.state.back
+        }
+        this.props.addNewCard(card);
+        this.setState({
+            front:'',
+            back:''
+        });
+    }
+
+    render() {
+        return (
+            <Segment>
+                <h3>Add new Flashcard</h3>
+                <form onSubmit={this.onSubmit}></form>
+                <FormInput
+                        label="Front"
+                        value={this.state.front}
+                        name="front"
+                        onChange={this.handleChange}
+                    />
+                <FormInput
+                        label="Back"
+                        value={this.state.back}
+                        name="back"
+                        onChange={this.handleChange}/>
+                <Button type="submit">Add to stack</Button>
+            </Segment>
+        );
+    }
+}
+
+export default class CollectionView extends Component {
+    constructor(props) {
+        super(props);
+        state = {
+            collection:[],
+            page: 1,
+            front: true
+        }
+    }
+
+    render(){
+        return(
+            <div className="collection-view">
+                <div>
+                    <Button
+                        primary onClick={() => {this.setState(state => ({
+                            page: Math.max(state.page -1, 1), front: true
+                        }))}}>Previous</Button>
+                    <Button
+                        primary onClick={() => {this.setState(state => ({
+                            page: Math.minb(state.page +1, this.state.collection.length), front: true
+                        }))}}>Next</Button>
+                    <Button
+                        secondary onClick={() => {this.setState(state => ({front: !state.front}))}}>Flip</Button>
+                </div>
+            </div>
+        )
     }
 }
